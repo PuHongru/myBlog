a169da9f66901f00131cc576f78454b640c41dfa
PuHongru <puhongruoffer@163.com>
2016-11-21 17:00:46 +0800

    博客样式

--- index.js
+++ index.js
@@ -28,14 +28,28 @@ app.use(session({
     secret: config.session.secret,  // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
     cookie: {
         maxAge: config.session.maxAge  // 过期时间，过期后 cookie 中的 session id 自动删除
-    }
-    //store: new MongoStore({  // 将 session 存储到 mongodb
-        //url: config.mongodb  // mongodb 地址
-    //})
+    },
+    store: new MongoStore({  // 将 session 存储到 mongodb
+        url: config.mongodb  // mongodb 地址
+    })
 }));
 
 // flash 中间件，用来显示通知
 app.use(flash());
+
+// 设置模板全局常量
+app.locals.blog = {
+    title: pkg.name,
+    description: pkg.description
+};
+
+// 添加模板必须的三个常量
+app.use(function (req, res, next) {
+    res.locals.user = req.session.user;
+    res.locals.success = req.flash('success').toString();
+    res.locals.error = req.flash('error').toString();
+    next();
+});
 
 // 路由
 routes(app);
